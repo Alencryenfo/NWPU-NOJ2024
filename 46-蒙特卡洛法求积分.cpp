c497fb568bb11adc240d08067c04259b1053c478
Alencryenfo <111328476+Alencryenfo@users.noreply.github.com>
2024-10-31 11:38:23 +0800

    Update 46-蒙特卡洛法求积分.cpp

--- 46-蒙特卡洛法求积分.cpp
+++ 46-蒙特卡洛法求积分.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
 #include <cmath>
-#include <random>
+#include <iomanip>
 using namespace std;
-#define _USE_MATH_DEFINES
-double func(int n , double x){
-    if (n == 1) {
-        return pow(x , 4)*pow(M_E , 0-x);
+int m,N;
+double a,b,ans;
+double func(int model,double x) {
+    if(model==1) {
+        return pow(x,4)*exp(-x);
     }
-    else if (n == 2){
-        return (x*x + 1);
+    if(model==2) {
+        return pow(x,2)+1;
     }
-    else if (n == 3){
+    if(model==3) {
         return cos(x);
     }
-    else if (n == 4){
+    if(model==4) {
         return sqrt(x)*(x-2);
     }
-    else if (n == 5){
-        return 2*sin(x) - 5*cos(x);
+    if(model==5) {
+        return 2*sin(x)-5*cos(x);
     }
-    return 0;
 }
 int main() {
-    srand(RAND_MAX);
-    int m , N;
-    double a , b;
-    cin >> m >> a >> b >> N;
-    double value = 0;
-    for (int i = 1 ; i <= N - 1 ; i ++){
-        //cout << a + (b-a)*((double)rand()/RAND_MAX) << " ";
-        //double x = a + (b-a)*((double)rand()/RAND_MAX);
-        //value += func(m , x) * (b - a);   
-        cout << rand() << endl; 
+    cin>>m>>a>>b>>N;
+    int tot=N;
+    srand(static_cast<unsigned int>(RAND_MAX));
+    //cout<<static_cast<double>(rand())/RAND_MAX;
+    N=1999;
+    while (N--) {
+        ans+=(func(m,(static_cast<double>(rand())/RAND_MAX)*(b-a)+a))*(b-a);
     }
-    printf("%.6f\n" , value/N);
-    //cout << (65.0/M_E)-(1569.0/pow(M_E,5));
+    cout<<fixed<<setprecision(6)<<ans/tot;
     return 0;
 }
