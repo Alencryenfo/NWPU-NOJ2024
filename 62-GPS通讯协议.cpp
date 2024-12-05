7a193413dfa0cac479d97f7fcb07e0f644593f21
Alencryenfo <111328476+Alencryenfo@users.noreply.github.com>
2024-12-05 11:47:43 +0800

    Update 62-GPS通讯协议.cpp

--- 62-GPS通讯协议.cpp
+++ 62-GPS通讯协议.cpp
@@ -1,49 +1,61 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+ 
 using namespace std;
-bool f(){
+ 
+string out[100];
+int k=0;
+ 
+int check(string str){
+    int i,result;
+ 
+    for(result=str[1],i=2;str[i]!='*';i++)
+    {
+        result^=str[i];
+    }
+    return result;
+}
+ 
+int convert(string str){
+    int res=0;
+    res=stoi(str,0,16);
+    return res;
+}
+ 
+ 
+void convert_BeingTime(string utcTime){
+    int  hour=stoi(utcTime.substr(0,2));
+    int  B_hour=(hour+8)%24;
+    if(B_hour/10==0)
+        out[k++]="0"+to_string(B_hour)+":"+utcTime.substr(2,2)+":"+utcTime.substr(4,2);
+    else
+        out[k++]=to_string(B_hour)+":"+utcTime.substr(2,2)+":"+utcTime.substr(4,2);
+ 
+}
+ 
+int main(){
     string str;
-    cin >> str;
-    if (str[0] == 'E' && str[1] == 'N' && str[2] == 'D' && str.length() == 3){
-        return false;
-    }
-    if (str[1] != 'G' || str[2] != 'P' || str[3] != 'R' || str[4] != 'M' || str[5] != 'C'){
-        return true;
-    }
-    int ans = str[1];
-    for (int i = 2 ; i < str.length() ; i ++){
-        if (str[i] == '*'){
-            int check = 0;
-            for (int j = i + 1 ; j < str.length() ; j ++){
-                if (str[j] >= 'A' && str[j] <= 'F'){
-                    check *= 16;
-                    check += str[j] - 'A' + 10;
+    while(cin>>str){
+        if(str=="END") break;
+        if(str.compare(0,6,"$GPRMC")==0){
+            size_t asteriskPos = str.find('*');
+            if(asteriskPos!=string::npos){
+                int checksum=check(str);
+                
+                int senchecksum=convert(str.substr(asteriskPos + 1, 2));
+                if(checksum!=senchecksum) {
+                    out[k++]="error";
+          
                 }
                 else{
-                    check *= 16;
-                    check += str[j] - '0';
+                    string utcTime = str.substr(7, 6);
+     
+                    convert_BeingTime(utcTime);
                 }
-                check %= 65536;
             }
-            if (check != ans){
-                cout << "error" << endl;
-                return true;
-            }
-            break;
         }
-        if (str[i] == ','){
-            continue;
-        }
-        ans = (ans^str[i]) % 65536;
+ 
     }
-    cout << ((str[8] - '0') + ((str[7] - '0') * 10) + 8) % 24 << ":";
-    cout << (str[10] - '0') + ((str[9] - '0') * 10) << ":";
-    cout << (str[12] - '0') + ((str[11] - '0') * 10) << endl;
-    return true;
-}
-int main(){
-    while(f()){
-        
+    for(int i=0;i<k;i++){
+        cout<<out[i]<<endl;
     }
-    return 0;
-}
+}//来自https://blog.csdn.net/annesede/article/details/133761873，使用了C++风格字符串，不知道为什么自己写的C风格没过，网上也没有C的代码，评测机NT
